28e520a29455f662474ef5fd842b0e853cdd2756
crazydrummer81 <48684620+crazydrummer81@users.noreply.github.com>
2023-01-06 14:10:02 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ztm-vue-course-section-9-84-music
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ztm-vue-course-section-9-84-music)
---
+++ src/main.js
@@ -0,0 +1,6 @@
+const { createApp } = require('vue');
+import App from './App.vue';
+
+import './assets/base.css';
+
+createApp(App).mount('#app');
